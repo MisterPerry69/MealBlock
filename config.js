0ea32d806e90cc4b16a052bfe0b63c316ebfcf11
MisterPerry69 <aledalcortivo@gmail.com>
2026-07-15 22:32:52 +0200

    asas

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Incolla qui l'URL del Web App di Google Apps Script dopo il deploy.
 window.MB_CONFIG = {
   // es. "https://script.google.com/macros/s/AKfycb.../exec"
-  gasUrl: "",
+  gasUrl: "https://script.google.com/macros/s/AKfycbzQuqTkVfUhAb2BxwEkN9_LI3ELk1R4vp5Ehx83-g8zQY6EoSVmNkImDuAh7eoG-kGsig/exec",
   // se true, usa dati mock locali senza chiamare GAS (utile in sviluppo/offline design)
-  useMock: true,
+  useMock: false,
 };
